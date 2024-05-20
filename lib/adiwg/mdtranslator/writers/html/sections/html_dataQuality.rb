3103634ba7015eaadc9117b0c69567ee9db07d11
Johnathan Aspinwall <jwaspin@gmail.com>
2024-05-20 10:39:09 -0600

    Refactor data quality report type display and add system identifier support

--- lib/adiwg/mdtranslator/writers/html/sections/html_dataQuality.rb
+++ lib/adiwg/mdtranslator/writers/html/sections/html_dataQuality.rb
@@ -16,6 +16,22 @@ module ADIWG
             scopeClass = Html_Scope.new(@html)
             dataQualityReportClass = Html_DataQualityReport.new(@html)
 
+            # system identifier
+            unless hDataQuality[:systemIdentifier].nil?
+              systemIdentifier = hDataQuality[:systemIdentifier]
+              @html.section(:class => 'block') do
+                @html.details do
+                  @html.summary('System Identifier', {'class' => 'h4'})
+                    @html.em('UUID: ')
+                    @html.text!(systemIdentifier[:uid])
+                    @html.br
+                    @html.em('Label:')
+                    @html.text!(systemIdentifier[:label])
+                    @html.br
+                end
+              end
+            end
+
             # scope
             unless hDataQuality[:scope].empty?
               @html.section(:class => 'block') do
