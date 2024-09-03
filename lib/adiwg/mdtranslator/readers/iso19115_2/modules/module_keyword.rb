705df0a091df8248728ae7000e5520f8bcfc5320
Reid Hewitt <sam.hew.r@gmail.com>
2024-09-03 13:09:18 -0600

    add keyword module; reader test; missing keyword test; iso19115-2 to dcatus keyword transform test

---
+++ lib/adiwg/mdtranslator/readers/iso19115_2/modules/module_keyword.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'nokogiri'
+require 'adiwg/mdtranslator/internal/internal_metadata_obj'
+
+module ADIWG
+   module Mdtranslator
+      module Readers
+         module Iso191152
+            module Keyword
+               @@keywordsXPath = 'gmd:MD_Keywords'
+               @@keywordXPath = 'gmd:keyword//gco:CharacterString'
+               @@keywordTypeXPath = 'gmd:type//gmd:MD_KeywordTypeCode'
+               @@thesaurusXPath = 'gmd:thesaurusName'
+               def self.unpack(xDescriptiveKeyword, hResponseObj)
+                  intMetadataClass = InternalMetadata.new
+                  hKeyword = intMetadataClass.newKeyword
+
+                  xMDKeywords = xDescriptiveKeyword.xpath(@@keywordsXPath)[0]
+                  return hKeyword if xMDKeywords.nil?
+
+                  # :keyword (required)
+                  # <xs:element name="keyword" type="gco:CharacterString_PropertyType" maxOccurs="unbounded"/>
+                  xKeywords = xMDKeywords.xpath(@@keywordXPath)
+                  if xKeywords.empty?
+                     msg = "ERROR: ISO19115-2 reader: element \'#{@@keywordXPath}\' is missing in #{xMDKeywords.name}"
+                     hResponseObj[:readerExecutionMessages] << msg
+                     hResponseObj[:readerExecutionPass] = false
+                     return hKeyword
+                  end
+
+                  xKeywords.each do |keyword|
+                     k = intMetadataClass.newKeywordObject
+                     k[:keyword] = keyword.text
+                     hKeyword[:keywords] << k
+                  end
+
+                  hKeyword
+               end
+            end
+         end
+      end
+   end
+end
