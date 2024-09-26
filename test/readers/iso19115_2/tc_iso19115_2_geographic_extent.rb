6faf03fc7fbbbeedbfa4dd8ba8dcfcb765558805
Jin-Sun-tts <jin.sun@gsa.gov>
2024-09-26 14:45:24 -0400

    fixed the class name

--- test/readers/iso19115_2/tc_iso19115_2_geographic_extent.rb
+++ test/readers/iso19115_2/tc_iso19115_2_geographic_extent.rb
@@ -5,7 +5,8 @@
 
 require 'adiwg/mdtranslator/readers/iso19115_2/modules/module_geographic_extent'
 require_relative 'iso19115_2_test_parent'
-class TestReaderIso191152Extent < TestReaderIso191152Parent
+
+class TestReaderIso191152GeographicExtent < TestReaderIso191152Parent
    @@nameSpace = ADIWG::Mdtranslator::Readers::Iso191152::GeographicExtent
 
    def test_geographic_extent_complete 
