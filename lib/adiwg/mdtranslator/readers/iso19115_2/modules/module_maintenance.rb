803b8f74866f53059f3c327136b10000c68ab4b7
Fuhu Xia <fxia@reisystems.com>
2024-10-03 11:44:17 -0400

    make frequency required

--- lib/adiwg/mdtranslator/readers/iso19115_2/modules/module_maintenance.rb
+++ lib/adiwg/mdtranslator/readers/iso19115_2/modules/module_maintenance.rb
@@ -8,7 +8,7 @@ module ADIWG
             module Maintenance
                @@maintenanceInfoXPath = 'gmd:MD_MaintenanceInformation'
                @@frequencyXPath = 'gmd:maintenanceAndUpdateFrequency/gmd:MD_MaintenanceFrequencyCode'
-               def self.unpack(xMaintenance, _hResponseObj)
+               def self.unpack(xMaintenance, hResponseObj)
                   intMetadataClass = InternalMetadata.new
                   hMaintenance = intMetadataClass.newMaintenance
 
@@ -16,9 +16,17 @@ module ADIWG
                   return nil if xMaintenanceInfo.nil?
 
                   # only thing dcatus needs
-                  # :frequency (optional)
+                  # :frequency (required)
                   # <xs:element name="maintenanceAndUpdateFrequency" type="gmd:MD_MaintenanceFrequencyCode_PropertyType"/>
                   xFrequency = xMaintenanceInfo.xpath(@@frequencyXPath)[0]
+                  if xFrequency.nil?
+                     msg = "WARNING: ISO19115-2 reader: element \'#{@@frequencyXPath}\' "\
+                        "is missing in #{xMaintenanceInfo.name}"
+                     hResponseObj[:readerExecutionMessages] << msg
+                     hResponseObj[:readerExecutionPass] = false
+                     return nil
+                  end
+
                   hMaintenance[:frequency] = xFrequency.nil? ? nil : xFrequency.attr('codeListValue')
 
                   # TODO: as needed
