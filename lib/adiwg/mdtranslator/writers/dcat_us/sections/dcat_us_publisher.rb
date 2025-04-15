5ce149cd3c361b1b39880593532843add413b795
Reid Hewitt <92057378+rshewitt@users.noreply.github.com>
2025-04-15 08:15:35 -0600

    update publisher access in dcatus writer; add contact handling in metadata info; fix tests and fixtures

--- lib/adiwg/mdtranslator/writers/dcat_us/sections/dcat_us_publisher.rb
+++ lib/adiwg/mdtranslator/writers/dcat_us/sections/dcat_us_publisher.rb
@@ -6,22 +6,31 @@ module ADIWG
           def self.build(intObj)
             contacts = intObj.dig(:contacts)
             metadata = intObj.dig(:metadata)
+
+            publisherAttempt1 = metadata.dig(:metadataInfo, :metadataContacts, 0, :parties, 0, :name)
+            unless publisherAttempt1.nil?
+              return Jbuilder.new do |json|
+                json.set!('@type', 'org:Organization')
+                json.set!('name', publisherAttempt1)
+              end
+            end
+
             responsible_parties = metadata&.dig(:resourceInfo, :citation, :responsibleParties)
             resource_distributions = metadata&.dig(:distributorInfo)
-            
+
             publisher = find_publisher(responsible_parties) || find_distributions_publisher(intObj)
-            
+
             return if publisher.nil?
-            
+
             name, org_name = extract_names(publisher, contacts)
-            
+
             return if name.nil?
-            
+
             build_json(name, org_name)
           end
-          
+
           private
-          
+
           def self.find_publisher(responsible_parties)
             responsible_parties&.detect do |party|
               party[:roleName] == 'publisher' && party.dig(:parties)&.first&.dig(:contactType) == 'organization'
@@ -38,26 +47,28 @@ module ADIWG
               end
             end
             nil
-          end            
-          
+          end
+
           def self.extract_names(publisher, contacts)
             contact_id = publisher.dig(:parties)&.first&.dig(:contactId) || publisher[:contactId]
             related_contact = contacts&.detect { |contact| contact[:contactId] == contact_id }
             org_name = nil
             if related_contact
               member_of_org_ids = related_contact[:memberOfOrgs]
-              member_of_orgs = contacts.select { |contact| member_of_org_ids&.include?(contact[:contactId]) && contact[:isOrganization] }
+              member_of_orgs = contacts.select do |contact|
+                member_of_org_ids&.include?(contact[:contactId]) && contact[:isOrganization]
+              end
               org_name = member_of_orgs.first&.dig(:name)
             end
             name = publisher.dig(:parties)&.first&.dig(:contactName) || publisher[:contactName]
             [name, org_name]
           end
-          
+
           def self.build_json(name, org_name)
             Jbuilder.new do |json|
               json.set!('@type', 'org:Organization')
               json.set!('name', name)
-              
+
               if org_name
                 json.subOrganizationOf do |json_sub|
                   json_sub.set!('@type', 'org:Organization')
@@ -66,7 +77,6 @@ module ADIWG
               end
             end
           end
-
         end
       end
     end
